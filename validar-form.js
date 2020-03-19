cdeeb4d20033754ff0c21e7901a53ee2cdefb6cd
arthuralecsander <50452924+arthuralecsander@users.noreply.github.com>
2020-03-19 15:23:59 -0300

    V0.4.2
    Correcao do Js e adicao de funcoes.

--- validar-form.js
+++ validar-form.js
@@ -1,9 +1,36 @@
 function Validar(){
-    var fNome = formulario.nome.value;
+    var formu = document.forms['formulario'];
+    var fNome = formu.nome.value;
+    var fTel = formu.tel.value;
 
-    if(fnome == "a"){
-        alert('Erro ao preencher');
-        formulario.nome.focus();
-        return false;
+
+    var error = false;
+    if(fNome == ""){
+        alert('Campo nao pode ser vazio');
+        formu.nome.focus();
+        error = true;
+        
+    }else if(fNome.indexOf(" ")==-1){
+        alert('Preencha com pelo menos um sobrenome');
+        error=true;
     }
+    if(fTel.length != 11){
+        alert('Telefone invalido');
+        error=true;
+
+    }else if(fTel.substring(2,3)!="-"){
+        alert('Formato de telefone incorreto xx-xxxxxxxx');
+        error=true;
+
+    }
+    if(error){
+        return false;
+    }else{
+        return true;
+        PostEnviar();
+        
+    }
+}
+function PostEnviar(){
+
 }
